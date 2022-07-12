6491d545f2dce2a920676908aa6626b0696e0743
Sihuan Newrise <sihuannewrise@gmail.com>
2022-07-12 20:16:24 +0300

    deploy with ssh5

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Ups, I did it again!')
 
 
 def second_page(request):
